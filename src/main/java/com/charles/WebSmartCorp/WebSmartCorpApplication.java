b18b30a6e60f2317fb2ffad4ccffb446eee05889
charlescrg <charles_crg@hotmail.com>
2022-05-02 18:32:50 -0300

    projeto criado

---
+++ src/main/java/com/charles/WebSmartCorp/WebSmartCorpApplication.java
@@ -0,0 +1,13 @@
+package com.charles.WebSmartCorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSmartCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSmartCorpApplication.class, args);
+	}
+
+}

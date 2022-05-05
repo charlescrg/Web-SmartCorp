abcf8c94d8dd8410e5b9c1725424be0ad692b29e
charlescrg <charles_crg@hotmail.com>
2022-05-05 14:52:48 -0300

    enum

--- src/main/java/com/charles/WebSmartCorp/entities/Order.java
+++ src/main/java/com/charles/WebSmartCorp/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.charles.WebSmartCorp.entities.enums.OrderStatus;
 import com.couchbase.client.core.deps.com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -30,14 +31,17 @@ public class Order implements Serializable{
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	private Integer orderStatus;
+	
 	public Order() {
 		
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);;
 		this.client = client;
 	}
 
@@ -64,8 +68,19 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
 
 	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 	@Override
 	public int hashCode() {

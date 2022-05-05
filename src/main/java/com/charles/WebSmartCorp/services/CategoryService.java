1307df201c544600ccae22e4be4888d1ab21fadf
charlescrg <charles_crg@hotmail.com>
2022-05-05 15:04:47 -0300

    categoria

---
+++ src/main/java/com/charles/WebSmartCorp/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.charles.WebSmartCorp.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.charles.WebSmartCorp.entities.Category;
+import com.charles.WebSmartCorp.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findByID(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}

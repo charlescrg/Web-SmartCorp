37cf3e5fe0f7eb5e867a0652daaa61dee635e96d
charlescrg <charles_crg@hotmail.com>
2022-05-06 14:26:33 -0300

    delete e update user

--- src/main/java/com/charles/WebSmartCorp/services/UserService.java
+++ src/main/java/com/charles/WebSmartCorp/services/UserService.java
@@ -3,11 +3,17 @@ package com.charles.WebSmartCorp.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.charles.WebSmartCorp.entities.User;
 import com.charles.WebSmartCorp.repositories.UserRepository;
+import com.charles.WebSmartCorp.services.exceptions.DatabaseException;
+import com.charles.WebSmartCorp.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -25,6 +31,32 @@ public class UserService {
 	
 	public User insert(User obj) {
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 
 }
